fa2791e194c288786af6ad4b6d0d7880f1c9e342
Kirill <kirill_zp@mail.ua>
2016-11-11 15:55:25 +0200

    v1.16 spisok del

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get '/all' => 'welcome#all'
+
   devise_for :sellers, controllers: {
       sessions: "sellers/sessions",
       registrations: "sellers/registrations",
