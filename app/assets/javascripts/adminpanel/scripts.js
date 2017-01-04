1923bb21ad371d9f204b1133ae03f1c18d991762
ekzema <xxx.zp@mail.ru>
2017-01-04 20:41:15 +0200

    fix  hide menu(problem turbolinks)

--- app/assets/javascripts/adminpanel/scripts.js
+++ app/assets/javascripts/adminpanel/scripts.js
@@ -66,26 +66,28 @@ function initializeJS() {
         jQuery(window).on('resize', responsiveView);
     });
 
-    jQuery('.toggle-nav').click(function () {
-        if (jQuery('#sidebar > ul').is(":visible") === true) {
-            jQuery('#main-content').css({
-                'margin-left': '0px'
-            });
-            jQuery('#sidebar').css({
-                'margin-left': '-180px'
-            });
-            jQuery('#sidebar > ul').hide();
-            jQuery("#container").addClass("sidebar-closed");
-        } else {
-            jQuery('#main-content').css({
-                'margin-left': '180px'
-            });
-            jQuery('#sidebar > ul').show();
-            jQuery('#sidebar').css({
-                'margin-left': '0'
-            });
-            jQuery("#container").removeClass("sidebar-closed");
-        }
+    document.addEventListener("turbolinks:load", function() {
+        jQuery('.toggle-nav').click(function () {
+            if (jQuery('#sidebar > ul').is(":visible") === true) {
+                jQuery('#main-content').css({
+                    'margin-left': '0px'
+                });
+                jQuery('#sidebar').css({
+                    'margin-left': '-180px'
+                });
+                jQuery('#sidebar > ul').hide();
+                jQuery("#container").addClass("sidebar-closed");
+            } else {
+                jQuery('#main-content').css({
+                    'margin-left': '180px'
+                });
+                jQuery('#sidebar > ul').show();
+                jQuery('#sidebar').css({
+                    'margin-left': '0'
+                });
+                jQuery("#container").removeClass("sidebar-closed");
+            }
+        });
     });
 
     //bar chart
