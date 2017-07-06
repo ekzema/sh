7e6c3df031bbd88a95319805acbb3c010a02c4d9
ekzema <xxx.zp@mail.ru>
2017-07-07 00:33:41 +0300

    add liveinternet

--- app/models/product_slide_image.rb
+++ app/models/product_slide_image.rb
@@ -1,6 +1,6 @@
 class ProductSlideImage < ApplicationRecord
   belongs_to :product
 
-  has_attached_file :image, :default_url => "noimage.png"
+  has_attached_file :image, :styles => {:original => ":1200", :medium => ":250"}, :default_url => "noimage.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
