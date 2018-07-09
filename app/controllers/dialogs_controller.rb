30d0325db229ba81217179b14e51501e0d45187f
ekzema <xxx.zp@mail.ru>
2018-07-10 01:05:16 +0300

    update json to message

--- app/controllers/dialogs_controller.rb
+++ app/controllers/dialogs_controller.rb
@@ -2,22 +2,29 @@ class DialogsController < ApplicationController
   before_action :authenticate_seller!
 
   def create
-    result = {success: false, message: "Возникла ошибка при отправке сообщения"}
+    @result = {success: false, message: "Возникла ошибка при отправке сообщения"}
     if params[:message][:body].empty?
-      result['message'] = "Сообщение не может быть пустым!"
-      return render json: result
+      @result[:message] = "Сообщение не может быть пустым!"
+      return  @result
     end
-    dialog = Dialog.create
-    message = Message.new(message_params)
-    message.dialog_id = dialog.id
-    if message.save
-      result[:success] = true
-      result[:message] = "Сообщение было успешно отправлено"
+    findmessage = Message.where(seller_id: params[:message][:seller_id], recipient_id: params[:message][:recipient_id]).take
+    if ! findmessage
+      dialog = Dialog.create
+      message = Message.new(message_params)
+      message.dialog_id = dialog.id
     else
-      dialog.destroy
+      message = Message.new(message_params)
+      message.dialog_id = findmessage.dialog_id
     end
-    result.to_json
-
+    if message.save
+      @result[:success] = true
+      @result[:message] = "Сообщение было успешно отправлено"
+    else
+      if dialog.defined?
+        dialog.destroy
+      end
+    end
+    @result
   end
 
   def destroy
